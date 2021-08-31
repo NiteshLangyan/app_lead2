83a4b751429ea01c378f2f31c38e40d2a162425b
root <root@localhost.localdomain>
2021-08-31 17:50:33 +0000

    -- initial commit

---
+++ main.js
@@ -0,0 +1,79 @@
+const dotenv = require('dotenv');
+dotenv.config();
+const express = require('express');
+const session = require('express-session');
+const path = require('path');
+const MongoDBStore = require('connect-mongodb-session')(session);
+const csrf = require('csurf');
+const compression = require('compression');
+const helmet = require('helmet');
+
+const mongoConnect = require('./utils/database').MongoConnect;
+
+const authRoutes = require('./routes/auth');
+const userRoutes = require('./routes/user');
+const adminRoutes = require('./routes/admin');
+const errorcontroller = require('./controllers/errors');
+
+const store = new MongoDBStore({
+    // uri: `mongodb+srv://${process.env.DATABASE_USER_NAME}:${process.env.DATABASE_USER_PASSWORD}@cluster0.7ow4b.mongodb.net/${process.env.DATABASE_NAME}?retryWrites=true&w=majority`,
+    uri: 'mongodb://127.0.0.1:27017/leads',
+    collection: 'sessions'
+});
+
+const server = express();
+
+server.set('view engine', 'ejs');
+server.set('views', 'views');
+
+server.use(
+    helmet.contentSecurityPolicy({
+        directives: {
+        defaultSrc: ["'self'", "https://js.stripe.com/"],
+        fontSrc: [
+            "'self'", 
+            "https://fonts.gstatic.com", 
+            "https://maxcdn.bootstrapcdn.com",
+            "https://cdn.jsdelivr.net/npm/bootstrap-icons@1.5.0/font/fonts/"
+        ],
+        scriptSrc: ["'self'", "'unsafe-inline'", "https://js.stripe.com/v3/"],
+        objectSrc: ["'none'"],
+        imgSrc: ["'self'", "https:", "data:"],
+        styleSrc: [
+            "'self'", 
+            "https://fonts.googleapis.com", 
+            "https://maxcdn.bootstrapcdn.com", 
+            "https://cdn.jsdelivr.net/npm/bootstrap-icons@1.5.0/font/bootstrap-icons.css"
+        ],
+        upgradeInsecureRequests: [],
+        },
+    })
+);
+server.use(compression());
+
+server.use(express.urlencoded({extended: true}));
+server.use(express.static(path.join(__dirname, 'public')));
+
+server.use(
+    session({
+      secret: 'my secret',
+      resave: false,
+      saveUninitialized: false,
+      store: store,
+      cookie: { secure: false, maxAge: 18000000 },
+    })
+);
+
+const csrfProtection = csrf();
+
+server.use(csrfProtection);
+
+server.use('/admin', adminRoutes);
+server.use(authRoutes);
+server.use(userRoutes);
+
+server.use(errorcontroller.get404);
+
+mongoConnect(() => {
+    server.listen(process.env.PORT || 3000);
+});
---
+++ utils/database.js
@@ -0,0 +1,30 @@
+const mongodb = require('mongodb');
+const MongoClient = mongodb.MongoClient;
+
+let _db;
+
+const MongoConnect = callback =>{
+    MongoClient.connect(
+        // `mongodb+srv://${process.env.DATABASE_USER_NAME}:${process.env.DATABASE_USER_PASSWORD}@cluster0.7ow4b.mongodb.net/${process.env.DATABASE_NAME}?retryWrites=true&w=majority`,
+        'mongodb://127.0.0.1:27017/leads',
+        { useUnifiedTopology: true }
+        )
+        .then(client => {
+            console.log("Connected");
+            _db = client.db();
+            callback();
+        })
+        .catch(err => {
+            console.log(err)
+            throw err;
+        });
+};
+
+const getdb = () => {
+    if(_db)
+        return _db;
+    else throw "no db found";
+}
+
+exports.MongoConnect = MongoConnect;
+exports.getdb = getdb;
